c1770628fb1a71259ae16057e588cca00a84f1fa
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-06-12 20:58:21 -0700

    This isnt fun lol

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
 var letterssmall = 'abcdefghijklmnopqrstuvwxyz' .split('');
-console.log(letterssmall);
-
 var lettersbig = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' .split('');
-console.log(lettersbig);
-
 var numbers = '123456789' .split('');
-console.log(numbers);
-
 var special = '©$¢‰@÷ƒ#‘❝≈~Ω' .split('');
-console.log(special);
 
 
+function generatePassword(){
+
+    var password = "";
+    var passwordChar ="";
+
+    var passwordlength = prompt("Password must be between 8-128 characters.")
+    passwordlength = parseInt(passwordlength);
+
+    if(passwordlength < 8){
+        alert("Password is too short!");
+        return "";
+    }
+
+    if(passwordlength > 128){
+        alert("Password is too long!");
+        return "";
+    }
+
+    var letterssmall = confirm("Would you like to use lowercase letters?");
+
+    if(letterssmall) {
+        passwordchar += letterssmall;
+    }
+
+    var lettersbig = confirm("Would you like to use uppercase letter?")
+
+    if(lettersbig){
+        passwordchar += lettersbig;
+    }
+
+    var numbers = confirm("Would you like to use numbers?")
+
+    if(numbers){
+        passwordchar += numbers;
+    }
+
+    var special = confirm("Would you like to use special characters?");
+
+    if(special){
+        passwordchar =+ special;
+    }
+
+    
