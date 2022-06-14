4efb888fe714032856821dce0549816d11fa9c86
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-06-13 18:04:51 -0700

    Started over

--- script.js
+++ script.js
@@ -1,53 +1,36 @@
-var generateBtn = document.getElementById("#generate");
-
-
-
+// Character arrays
 var letterssmall = 'abcdefghijklmnopqrstuvwxyz' .split('');
 var lettersbig = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' .split('');
 var numbers = '123456789' .split('');
-var special = '©$¢‰@÷ƒ#‘❝≈~Ω' .split('');
+var special = '!@#$%^&*()_+' .split('');
+
+// Task to be done
+// Generate Password
+function generatePassword() {
+    prompt("Pick between 8-128 characters.");
+    // store answer in variable
+
+}
 
 
-function generatePassword(){
 
-    var password = "";
-    var passwordChar ="";
 
-    var passwordlength = prompt("Password must be between 8-128 characters.")
-    passwordlength = parseInt(passwordlength);
 
-    if(passwordlength < 8){
-        alert("Password is too short!");
-        return "";
-    }
 
-    if(passwordlength > 128){
-        alert("Password is too long!");
-        return "";
-    }
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-    var letterssmall = confirm("Would you like to use lowercase letters?");
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-    if(letterssmall) {
-        passwordchar += letterssmall;
-    }
+  passwordText.value = password;
 
-    var lettersbig = confirm("Would you like to use uppercase letter?")
+}
 
-    if(lettersbig){
-        passwordchar += lettersbig;
-    }
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
-    var numbers = confirm("Would you like to use numbers?")
 
-    if(numbers){
-        passwordchar += numbers;
-    }
 
-    var special = confirm("Would you like to use special characters?");
-
-    if(special){
-        passwordchar =+ special;
-    }
-
-    
