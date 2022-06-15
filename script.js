a1d2ae87a077d2f06d9504fd9a0e7245fc3a43f8
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-06-15 12:00:06 -0700

    didnt commit enough.

--- script.js
+++ script.js
@@ -1,36 +1,83 @@
-// Character arrays
-var letterssmall = 'abcdefghijklmnopqrstuvwxyz' .split('');
-var lettersbig = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' .split('');
-var numbers = '123456789' .split('');
-var special = '!@#$%^&*()_+' .split('');
-
-// Task to be done
-// Generate Password
 function generatePassword() {
-    prompt("Pick between 8-128 characters.");
-    // store answer in variable
 
-}
+  var letterssmall = 'abcdefghijklmnopqrstuvwxyz' .split('');
+  var lettersbig = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' .split('');
+  var numbers = '123456789' .split('');
+  var special = '!@#$%^&*()_+' .split('');
+  var characterchoices = [];
+
+  passwordlength = prompt("Enter desired lenght of password between 8-128.");
+  if (passwordlength < 8 || passwordlength > 128) {
+    return;
+  } else {
+      alert("Your password will be " + passwordlength );
+  }
+  
+  haslettersbig = confirm("Would you like to use uppercase letters?");
+    if(haslettersbig) {
+      var uselettersbig = alert("Uppercase letters confirmed.");
+    }
+    else {
+      alert("Uppercase letters declined.");
+    }
+
+  hasletterssmall = confirm("Would you like to use lowercase letters?");
+      if(hasletterssmall) {
+        var useletterssmall = alert("Lowercase letters confirmed.");
+      }  
+      else {
+        alert("Lowercase letters declined.");
+      }
+
+  hasnumbers = confirm("Would you like to use numbers?");
+      if(hasnumbers) {
+        var hasnumbers = alert("Numbers confirmed.");
+      }
+      else {
+        alert("Numbers declined.");
+      }    
+
+  hasspecial = confirm("Would you like to use special characters?")
+      if(hasspecial) {
+        var hasspecial = alert("Special characters confirmed.");
+      }
+      else {
+        alert("Special characters declined.");
+      }
+  
+      if (haslettersbig === false && hasletterssmall === false && hasnumbers === false && hasspecial === false) {
+        return "please select at least one character type!";
+      };
+
+      if (haslettersbig) {
+        characterchoices = characterchoices.concat(lettersbig);
+      }
+      if (hasletterssmall) {
+        characterchoices = characterchoices.concat(letterssmall);
+      }
+      if (hasnumbers) {
+        characterchoices =characterchoices.concat(hasnumbers);
+      }
+      if (hasspecial) {
+        characterchoices = characterchoices.concat(hasspecial);
+      }
 
 
+      let createdpassword = ""
+      for (let i = 0; i < passwordlength; i++) {
+        let rng = [Math.floor(Math.random() * characterchoices.length)];
+        createdpassword = createdpassword + characterchoices[rng];
+      }
+      return createdpassword;
+        }    ;
 
 
+    var generateBtn = document.querySelector("#generate");
 
+    function writepassword() {
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
+      passwordText.value = password;
+    }
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
+    generateBtn.addEventListener("click", writepassword);
